ed76feeb3856603844b032e75ccda7733540f3ef
Kumar Ramesh <rkm372197@gmail.com>
2023-02-28 15:45:57 +0500

    Added two imports in the GeoLocator class and modified the newInstance() method

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -7,6 +7,8 @@ import feign.gson.GsonDecoder;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.FieldNamingPolicy;
+import feign.Logger;
+import feign.slf4j.Slf4jLogger;
 
 public interface GeoLocator {
 
@@ -16,11 +18,14 @@ public interface GeoLocator {
     @RequestLine("GET /{ipOrHostName}")
     GeoLocation getGeoLocation(@Param("ipOrHostName") String ipOrHostName);
 
+
     static GeoLocator newInstance() {
         return Feign.builder()
                 .decoder(new GsonDecoder(new GsonBuilder()
                         .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
                         .create()))
+                .logger(new Slf4jLogger())
+                .logLevel(Logger.Level.FULL)
                 .target(GeoLocator.class, "https://reallyfreegeoip.org/json/");
     }
 
